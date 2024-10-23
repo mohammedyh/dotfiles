cf83334a2dc54721d53733742c6683ee7c88e006
mohammedyh <mohammedyousufhamid@gmail.com>
2024-10-23 21:29:21 +0100

    feat: disable treesitter auto install
    - Delete .git and .github folders for nvim kickstart config
    - Switch color theme to tokyo-night

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -706,13 +706,15 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
+    -- 'catppuccin/nvim',
+    'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin'
+      -- vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -815,7 +817,7 @@ require('lazy').setup({
         'vimdoc',
       },
       -- Autoinstall languages that are not installed
-      auto_install = true,
+      -- auto_install = true,
       highlight = {
         enable = true,
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
