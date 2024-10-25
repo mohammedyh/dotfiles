5b723c0368b78ca32ca778045a7f2119525a2206
mohammedyh <32526267+mohammedyh@users.noreply.github.com>
2024-10-25 11:18:57 +0100

    feat: add script to fetch and update all git repos

---
+++ .config/update_repos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+GREEN="\e[32m"
+NOCOLOR="\e[0m"
+
+cd "$HOME/Code" || (echo "$HOME/Code doesn't exist" && exit)
+# projects=$(fd ^.git$ --type=d --hidden --exclude=/public,/vendor)
+
+for project in $(fd ^.git$ --type=d --hidden --exclude=/public,/vendor); do
+	projectPath="${project%/.git/}"
+
+	printf "\n%brepo: %s%b\n" "$GREEN" "$projectPath" "$NOCOLOR"
+
+	cd "$projectPath" || exit
+
+  git fetch --all --prune
+  git pull
+
+	cd "$HOME/Code" || (echo "$HOME/Code doesn't exist" && exit)
+done
