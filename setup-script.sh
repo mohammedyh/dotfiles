4f9c82e9c274fd0e5a4b22472f870060c4cf1857
mohammedyh <mohammedyousufhamid@gmail.com>
2025-05-27 16:44:32 +0100

    feat: set temporary env var to install brew casks without quarantine
    - Add doggo and zoxide packages
    - Use $HOME variable instead of tilde for path consistency
    - Rename file from fresh-setup-script.sh to setup-script.sh

--- setup-script.sh
+++ setup-script.sh
@@ -4,7 +4,7 @@ CYAN="\033[1;36m"
 NOCOLOR="\033[0m"
 
 printf "
-  ${CYAN}Setup script to install apps and CLI utils on a fresh machine${NOCOLOR}
+  ${CYAN}Setup script to install apps and CLI utils on a new machine${NOCOLOR}
 "
 
 # Spotlight
@@ -38,6 +38,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install fnm
 brew install bat
 brew install delta
+brew install doggo
 brew install eza
 brew install exiftool
 brew install composer
@@ -55,30 +56,33 @@ brew install tlrc
 brew install mailpit
 brew install charmbracelet/tap/freeze
 brew install vhs
+brew install zoxide
 
+export HOMEBREW_CASK_OPTS="--no-quarantine"
 # Browsers
-brew install --cask firefox --no-quarantine
-brew install --cask google-chrome --no-quarantine
+brew install --cask firefox
+brew install --cask google-chrome
 
 # Dev Apps
-brew install --cask visual-studio-code --no-quarantine
-brew install --cask tableplus --no-quarantine
-brew install --cask iterm2 --no-quarantine
-brew install --cask dbngin --no-quarantine
-brew install --cask gitbutler --no-quarantine
+brew install --cask visual-studio-code
+brew install --cask tableplus
+brew install --cask iterm2
+brew install --cask dbngin
+brew install --cask gitbutler
 
 # Notes Apps
-brew install --cask obsidian --no-quarantine
-brew install --cask notion --no-quarantine
-brew install --cask ticktick --no-quarantine
+brew install --cask obsidian
+brew install --cask notion
+brew install --cask ticktick
 
 # Util Apps
-brew install --cask raycast --no-quarantine
-brew install --cask cleanshot --no-quarantine
-brew install --cask screen-studio --no-quarantine
-brew install --cask 1password --no-quarantine
-brew install --cask protonvpn --no-quarantine
+brew install --cask raycast
+brew install --cask cleanshot
+brew install --cask screen-studio
+brew install --cask 1password
+brew install --cask protonvpn
 
+export HOMEBREW_CASK_OPTS=""
 brew doctor
 
 # Disable Brew and Go analytics
@@ -95,48 +99,44 @@ gh auth setup-git
 printf "
   ${CYAN}Cloning dotfiles repo${NOCOLOR}
 "
-cd ~/Downloads
+cd "$HOME/Downloads"
 gh repo clone mohammedyh/dotfiles
 
-# copy iTerm2 config / preferences
-cp -f ~/Downloads/dotfiles/.config/iterm2/com.googlecode.iterm2.plist ~/Library/Preferences
+# Copy iTerm2 config / preferences
+cp -f "$HOME/Downloads/dotfiles/.config/iterm2/com.googlecode.iterm2.plist" "$HOME/Library/Preferences"
 
-# copy .config folder
+# Copy .config folder
 printf "
   ${CYAN}Copying files in .config/ and .zshrc to $HOME${NOCOLOR}
 "
-cp -R ~/Downloads/dotfiles/.config/* ~/.config
-rm -rf ~/.config/iterm2
-cp -f ~/Downloads/dotfiles/.zshrc $HOME
-cp -f ~/Downloads/dotfiles/.gitconfig $HOME
+cp -r "$HOME/Downloads/dotfiles/.config/*" "$HOME/.config"
+rm -rf "$HOME/.config/iterm2"
+cp -f "$HOME/Downloads/dotfiles/.zshrc" $HOME
+cp -f "$HOME/Downloads/dotfiles/.gitconfig" $HOME
 
-cd ..
+cd ../
 
-rm -rf ~/Downloads/dotfiles
-touch ~/.hushlogin
+rm -rf "$HOME/Downloads/dotfiles"
+touch "$HOME/.hushlogin"
+
+# Copy vscode settings.json, keybindings.json and language snippets
+cp -f "$HOME/Downloads/dotfiles/vscode/settings.json" "$HOME/Library/Application\ Support/Code/User/"
+cp -f "$HOME/Downloads/dotfiles/vscode/keybindings.json" "$HOME/Library/Application\ Support/Code/User/"
+cp -rf "$HOME/Downloads/dotfiles/vscode/snippets" "$HOME/Library/Application\ Support/Code/User/"
 
 # Install vscode extensions from list
 for extension in $(cat ./dotfiles/vscode/extensions.txt); do
   code --install-extension $extension
 done
 
-# Copy vscode settings.json, keybindings.json and language snippets
-cp -f ~/Downloads/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/
-cp -f ~/Downloads/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/
-cp -rf ~/Downloads/dotfiles/vscode/snippets ~/Library/Application\ Support/Code/User/
-
+# Install Node package runner helper
 go install "github.com/mohammedyh/npr@latest"
 
 # Import raycast settings using .rayconfig export file
 printf "
-  ${CYAN}Remember to import raycast settings using .rayconfig export file${NOCOLOR}
-"
-
-# Fonts
-printf "
   ${CYAN}Next steps:
 
-  - Install fonts: Dank Mono, MonoLisa, JetBrainsMono
-  - Import .rayconfig file to import Raycast setting
+  - Add fonts
+  - Import Raycast config
   ${NOCOLOR}
 "
