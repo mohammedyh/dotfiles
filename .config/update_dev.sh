969601cac83157e778dd7743b706df5c461464dc
mohammedyh <32526267+mohammedyh@users.noreply.github.com>
2025-08-09 15:43:46 +0000

    feat: silence neovim commands output
    Update the pnpm message when a new Node version is installed

--- .config/update_dev.sh
+++ .config/update_dev.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# See if any of this can be parallelised
-
 CYAN="\033[1;36m"
 YELLOW="\033[1;33m"
 NOCOLOR="\033[0m"
@@ -9,10 +7,6 @@ NOCOLOR="\033[0m"
 # Update npm and global packages
 printf "\n%bUpdating global npm packages%b\n" "$CYAN" "$NOCOLOR"
 npm update -g
-
-# Clear corepack cache
-# printf "%bClearing corepack cache%b\n" "$CYAN" "$NOCOLOR"
-# corepack cache clean
 
 # Update composer
 printf "\n%bUpdating composer%b\n" "$CYAN" "$NOCOLOR"
@@ -43,9 +37,9 @@ brew cleanup --prune=all -s
 
 # Update Lazy plugins, Mason and Treesitter
 printf "\n%bUpdate Lazy plugins, Mason and Treesitter%b\n" "$CYAN" "$NOCOLOR"
-nvim --headless "+Lazy! sync" +qa
-nvim --headless "+MasonUpdate" +qa
-nvim --headless -c "TSUpdate" -c "qa"
+nvim --headless "+Lazy! sync" +qa > /dev/null
+nvim --headless "+MasonUpdate" +qa > /dev/null
+nvim --headless -c "TSUpdate" -c "qa" > /dev/null
 
 # Clean cache and update TLDR pages
 printf "\n\n%bClean cache and update TLDR pages%b\n" "$CYAN" "$NOCOLOR"
@@ -60,17 +54,14 @@ nodeInstallOutput=$(fnm install --lts 2>&1)
 if [[ $nodeInstallOutput != *"Version already installed"* ]]; then
 	printf "%s" "$nodeInstallOutput"
 	fnm default lts-latest
-	printf "\n%bLTS version set as default. Remove any old Node versions%b\n" "$CYAN" "$NOCOLOR"
-	printf "\n%bTo activate pnpm run 'corepack enable' and 'corepack install --global pnpm@latest'%b\n" "$CYAN" "$NOCOLOR"
+	printf "\n%bLTS version set as default. Remove old Node versions%b\n" "$CYAN" "$NOCOLOR"
+	printf "\n%bNeed to re-install pnpm, run 'npm install -g pnpm'%b\n" "$CYAN" "$NOCOLOR"
 else
 	printf "\n%bLatest Node version already installed%b\n" "$YELLOW" "$NOCOLOR"
 fi
 
-# Check for System, Safari, and App Store updates
-# printf "\n%bCheck for System, Safari, and App Store updates%b\n" "$CYAN" "$NOCOLOR"
-# softwareupdate -ia
-
-printf "\n%bCheck system for potential (brew) problems%b\n" "$CYAN" "$NOCOLOR"
+printf "\n%bCheck system for potential brew problems%b\n" "$CYAN" "$NOCOLOR"
 brew doctor
 
 printf "\n%bUpdates complete%b\n" "$CYAN" "$NOCOLOR"
+
