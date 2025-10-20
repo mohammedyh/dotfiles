7a56b6eb9931c130de7d0d689b0aee31aa52989d
mohammedyh <mohammedyousufhamid@gmail.com>
2025-10-21 00:18:01 +0100

    feat(scripts): add bun support and improve update scripts
    Adds support for the bun package manager and updating Neovim Mason
    tools. The shell script output is refactored for better readability, and
    golangci-lint is now skipped during the Go tools update process.

--- .config/update_dev.sh
+++ .config/update_dev.sh
@@ -4,64 +4,76 @@ CYAN="\033[1;36m"
 YELLOW="\033[1;33m"
 NOCOLOR="\033[0m"
 
+print() {
+  printf "\n%b%s%b\n" "$CYAN" "$1" "$NOCOLOR"
+}
+
+print_warning() {
+  printf "\n%b%s%b\n" "$YELLOW" "$1" "$NOCOLOR"
+}
+
 # Update npm and global packages
-printf "\n%bUpdating global npm packages%b\n" "$CYAN" "$NOCOLOR"
+print "Updating bun, npm and global packages"
 npm update -g
+bun upgrade
+bun -g update
 
 # Update composer
-printf "\n%bUpdating composer%b\n" "$CYAN" "$NOCOLOR"
+print "Updating composer"
 composer self-update
 
 # Update global composer packages
-printf "\n%bUpdating global composer packages%b\n" "$CYAN" "$NOCOLOR"
+print "Updating global composer packages"
 composer global update
 
 # Clear npm and pnpm cache
-printf "\n%bClearing npm and pnpm cache%b\n" "$CYAN" "$NOCOLOR"
+print "Clearing npm and pnpm cache"
 pnpm store prune
 npm cache clean --force
+bun -g pm cache rm
 
 # Clear composer cache
-printf "\n%bClearing composer cache%b\n" "$CYAN" "$NOCOLOR"
+print "Clearing composer cache"
 composer clear-cache
 
 # Update Homebrew and formulae
-printf "\n%bUpdating Homebrew and all formulae%b\n" "$CYAN" "$NOCOLOR"
+print "Updating Homebrew and all formulae"
 brew update
 brew upgrade
 
 # Remove unused formulae and clear Homebrew cache
-printf "\n%bRemove unused formulae and clear all Homebrew cache%b\n" "$CYAN" "$NOCOLOR"
+print "Remove unused formulae and clear Homebrew cache"
 brew autoremove
 brew cleanup --prune=all -s
 
 # Update Lazy plugins, Mason and Treesitter
-printf "\n%bUpdate Lazy plugins, Mason and Treesitter%b\n" "$CYAN" "$NOCOLOR"
+print "Update Lazy plugins, Mason and Treesitter"
 nvim --headless "+Lazy! sync" +qa > /dev/null
 nvim --headless "+MasonUpdate" +qa > /dev/null
+nvim --headless "+MasonToolsUpdate" +qa > /dev/null
 nvim --headless -c "TSUpdate" -c "qa" > /dev/null
 
 # Clean cache and update TLDR pages
-printf "\n\n%bClean cache and update TLDR pages%b\n" "$CYAN" "$NOCOLOR"
-tldr --clean-cache
+print "Clear TLDR cache and update pages"
+echo "y" | tldr --clean-cache
 tldr --update
 
 # Install LTS version of Node
-printf "\n%bInstalling LTS version of Node%b\n" "$CYAN" "$NOCOLOR"
-printf "%bCurrent Node version: $(fnm current)%b\n" "$CYAN" "$NOCOLOR"
+print "Installing LTS version of node"
+print "Current node version: $(fnm current)"
 nodeInstallOutput=$(fnm install --lts 2>&1)
 
 if [[ $nodeInstallOutput != *"Version already installed"* ]]; then
 	printf "%s" "$nodeInstallOutput"
 	fnm default lts-latest
-	printf "\n%bLTS version set as default. Remove old Node versions%b\n" "$CYAN" "$NOCOLOR"
-	printf "\n%bNeed to re-install pnpm, run 'npm install -g pnpm'%b\n" "$CYAN" "$NOCOLOR"
+  print "LTS version set as default"
+	print "Remember to re-install pnpm"
 else
-	printf "\n%bLatest Node version already installed%b\n" "$YELLOW" "$NOCOLOR"
+	print_warning "Latest Node version already installed"
 fi
 
-printf "\n%bCheck system for potential brew problems%b\n" "$CYAN" "$NOCOLOR"
+print "Check system for potential brew problems"
 brew doctor
 
-printf "\n%bUpdates complete%b\n" "$CYAN" "$NOCOLOR"
+print "Updates complete"
 
--- .config/update_go_tools.sh
+++ .config/update_go_tools.sh
@@ -4,22 +4,26 @@ export GOPROXY='direct'
 
 RED="\033[30m"
 GREEN="\033[32m"
+BLUE_HIGHLIGHT="\033[34;7m"
 BLUE="\033[34m"
-BLUEHIGHLIGHT="\033[34;7m"
 NOCOLOR="\033[0m"
 
-printf "%b Updating Go binaries \n\n%b" "$BLUEHIGHLIGHT" "$NOCOLOR"
+printf "%b Updating Go binaries %b\n\n" "$BLUE_HIGHLIGHT" "$NOCOLOR"
 
 for bin in $(ls $HOME/go/bin); do
-    printf "%bUpdating $bin%b\n" "$GREEN" "$NOCOLOR"
-    path=$(go version -m "$HOME/go/bin/$bin" | rg 'path' --stop-on-nonmatch | awk '{print $2}')
+  if [ "$bin" == "golangci-lint" ]; then
+    continue
+  fi
 
-    if [ -z "$path" ]; then
-        $printf "%bCouldn't get path for binary%b\n" "$RED" "$NOCOLOR"
-        exit 1
-    fi
+  printf "%bUpdating $bin%b\n" "$GREEN" "$NOCOLOR"
+  path=$(go version -m "$HOME/go/bin/$bin" | rg 'path' --stop-on-nonmatch | awk '{print $2}')
 
-    go install "$path@latest"
+  if [ -z "$path" ]; then
+      $printf "%bCouldn't get path for binary%b\n" "$RED" "$NOCOLOR"
+      exit 1
+  fi
+
+  go install "$path@latest"
 done
 
 printf "\n%bGo binaries updated!%b\n" "$BLUE" "$NOCOLOR"
