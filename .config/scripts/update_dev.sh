8bbc77fd016730be60169468de7d643ec1538c85
mohammedyh <mohammedyousufhamid@gmail.com>
2026-03-29 15:31:28 +0100

    chore: update dev environment maintenance script
    - Install latest version of global bun packages
    - Comment out global composer package updates
    - Install and switch to the latest lts node with `--use`
    - Remove corepack and reinstall pnpm after LTS node setup

--- .config/scripts/update_dev.sh
+++ .config/scripts/update_dev.sh
@@ -16,18 +16,18 @@ print_warning() {
 print "Updating bun, npm and global packages"
 npm update -g
 bun upgrade
-bun -g update
+bun -g update --latest
 
 # Update composer
 print "Updating composer"
 composer self-update
 
 # Update global composer packages
-print "Updating global composer packages"
-composer global update
+# print "Updating global composer packages"
+# composer global update
 
-# Clear npm and pnpm cache
-print "Clearing npm and pnpm cache"
+# Clearing npm, pnpm and bun cache
+print "Clearing npm, pnpm and bun cache"
 pnpm store prune
 npm cache clean --force
 bun -g pm cache rm
@@ -42,37 +42,39 @@ brew update
 brew upgrade
 
 # Remove unused formulae and clear Homebrew cache
-print "Remove unused formulae and clear Homebrew cache"
+print "Removing unused formulae and clear Homebrew cache"
 brew autoremove
 brew cleanup --prune=all -s
 
 # Update Lazy plugins, Mason and Treesitter
-print "Update Lazy plugins, Mason and Treesitter"
+print "Updating Lazy plugins, Mason and Treesitter"
 nvim --headless "+Lazy! sync" +qa > /dev/null
 nvim --headless "+MasonUpdate" +qa > /dev/null
 nvim --headless "+MasonToolsUpdate" +qa > /dev/null
 nvim --headless -c "TSUpdate" -c "qa" > /dev/null
 
 # Clean cache and update TLDR pages
-print "Clear TLDR cache and update pages"
+print "Clearing TLDR cache and update pages"
 echo "y" | tldr --clean-cache
 tldr --update
 
 # Install LTS version of Node
 print "Installing LTS version of node"
 print "Current node version: $(fnm current)"
-nodeInstallOutput=$(fnm install --lts 2>&1)
+nodeInstallOutput=$(fnm install --lts --use 2>&1)
 
 if [[ $nodeInstallOutput != *"Version already installed"* ]]; then
 	printf "%s" "$nodeInstallOutput"
 	fnm default lts-latest
-  print "LTS version set as default"
-	print "Remember to re-install pnpm"
+  print "LTS version set as default, remember to remove old version(s)"
+	print "Removing corepack and installing pnpm"
+  npm -g rm corepack
+  npm -g i pnpm
 else
 	print_warning "Latest Node version already installed"
 fi
 
-print "Check system for potential brew problems"
+print "Checking system for potential brew problems"
 brew doctor
 
 print "Updates complete"
